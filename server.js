e4b1c4119a6f811b2c7b75a157fe1e4f32b67884
rellcodes16 <devrellmara@gmail.com>
2025-10-26 09:28:00 -0700

    new changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 dotenv.config({ path: './.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE;
 
 mongoose.connect(DB)
   .then(() => console.log('DB connection successful'))
