8b66178079d1a822eb96317ed1063f8ec22c7412
rellcodes16 <devrellmara@gmail.com>
2025-11-07 18:08:26 +0100

    new changes

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-// localServer.js
-const app = require('./app');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
-dotenv.config();
+dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE;
-mongoose.connect(DB)
-  .then(() => console.log('DB connected'))
-  .catch((err) => console.error('DB connection error:', err));
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-const port = process.env.PORT || 3000;
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) => {
+    console.error('DB connection error:', err.message);
+  });
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+  console.log(`Server running on port ${port}...`);
 });
